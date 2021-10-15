0f50589f7a989a3d94325c4282011dd6bbb13224
IAmMasterCraft <bolu.akinsefunmi@gmail.com>
2021-10-15 14:05:15 +0100

    a lot of this and that

--- app.js
+++ app.js
@@ -1,76 +1,71 @@
-let menu = document.querySelector('#menu-icon')
-let navbar = document.querySelector('.navbar')
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
 
-menu.onclick = () =>{
-    navbar.classList.toggle('active')
-}
+menu.onclick = () => {
+  navbar.classList.toggle("active");
+};
 
-window.onscroll = () =>{
-    navbar.classList.remove('active')
-}
+window.onscroll = () => {
+  navbar.classList.remove("active");
+};
 
 const feature = [
-    {
-        name: "ipad 2020",
-        cost: 200,
-        images: ".images/ipadjpg.jpg"
-    },
-    {
-        name: "iPhone 11 Pro",
-        cost: 150,
-        images: "./images/phone.jpg"
-    },
-    {
-        name: "Haier Freezer",
-        cost: 350,
-        images: ".images/freezer.jpg"
-    },
-    {
-        name: "Television",
-        cost: 250,
-        images: "./images/television.jpg"
-    },
-    {
-        name: "Sweat-Shirt",
-        cost: 20,
-        images: "./images/shirt1.jpg"
-    }
-]
-const shop = [
-    {
+  {
+    name: "ipad 2020",
+    cost: 200,
+    images: ".images/ipadjpg.jpg",
+  },
+  {
+    name: "iPhone 11 Pro",
+    cost: 150,
+    images: "./images/phone.jpg",
+  },
+  {
+    name: "Haier Freezer",
+    cost: 350,
+    images: ".images/freezer.jpg",
+  },
+  {
+    name: "Television",
+    cost: 250,
+    images: "./images/television.jpg",
+  },
+  {
+    name: "Sweat-Shirt",
+    cost: 20,
+    images: "./images/shirt1.jpg",
+  },
+];
+const shop = [{}];
 
-    }
-]
-
-var removeItem = document.getElementsByClassName("btn-remove")
-for (var i =0; i<removeItem.length; i++){
-    var button = removeItem[i]
-    button.addEventListener('click', removeCartItem) 
+var removeItem = document.getElementsByClassName("btn-remove");
+for (var i = 0; i < removeItem.length; i++) {
+  var button = removeItem[i];
+  button.addEventListener("click", removeCartItem);
 }
 
-var quantityInputs = document.getElementsByClassName("cart-quantity-input")
-for (var i =0; i<quantityInputs.length; i++){
-    var input = quantityInputs[i]
-    input.addEventListener('click', quantityChanges)
+var quantityInputs = document.getElementsByClassName("cart-quantity-input");
+for (var i = 0; i < quantityInputs.length; i++) {
+  var input = quantityInputs[i];
+  input.addEventListener("click", quantityChanges);
 }
 
 //purchase function
-function purchase(){
-    alert('Thanks for shopping with us')
-    var cartItems = document.getElementsByClassName('cart-items')[0]
-    while (cartItems.hasChildNodes()){
-        cartItems.removeChild(cartItems.firstChild)
-    }
-    updateCartTotal()
+function purchase() {
+  alert("Thanks for shopping with us");
+  var cartItems = document.getElementsByClassName("cart-items")[0];
+  while (cartItems.hasChildNodes()) {
+    cartItems.removeChild(cartItems.firstChild);
+  }
+  updateCartTotal();
 }
 //document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchase)
 
-
 //function to display featured products to the screen
-const container = document.querySelectorAll(".shop-container")
-function featuredProduct(Items){
-    let displayProducts = Items.map(function(item){
-        return `<div class="box">
+const container = document.querySelectorAll(".shop-container");
+function featuredProduct(Items) {
+  let displayProducts = Items.map(function (item) {
+    return `<div class="box">
         <div class="box-img">
           <img src=${item.images} alt="apple" class="image">
         </div>
@@ -79,120 +74,161 @@ function featuredProduct(Items){
         <span class="price">${item.cost}</span>
         <button class="add">add</button>
         </div>
-      </div>`
-     })
-    displayProducts = displayProducts.join("")
-    container.innerHTML = displayMenu
-    console.log(displayProducts)
+      </div>`;
+  });
+  displayProducts = displayProducts.join("");
+  container.innerHTML = displayMenu;
+  console.log(displayProducts);
 }
 
 //removing items added to the cart
-function removeCartItem(event){
-    var buttonClickedd = event.target
-    buttonClickedd.parentElement.parentElement.remove()
-    updateCartTotal()
+function removeCartItem(id) {
+  // var buttonClickedd = event.target
+  // buttonClickedd.parentElement.parentElement.remove()
+  // updateCartTotal()
+
+  // read data from local storage
+  const cart = JSON.parse(localStorage.getItem("products"));
+  // remove product from cart
+  const updateCart = cart.filter((prod, index) => index !== Number(id));
+  // update local storage data
+  localStorage.setItem("products", JSON.stringify(updateCart));
+  // empty cart-items div
+  document.getElementById("cart-items").innerHTML = "";
+  // update cart page
+  displayCart();
 }
 
-//checks for the value of the quantity 
-function quantityChanges(event){
-    var input = event.target
-    if(isNaN(input.value) || input.value <= 0){
-        input.value = 1
-     }
-    subtotal()
-    updateCartTotal()
+//checks for the value of the quantity
+function quantityChanges(event) {
+  var input = event.target;
+  if (isNaN(input.value) || input.value <= 0) {
+    input.value = 1;
+  }
+  subtotal();
+  updateCartTotal();
 }
 
-
-const addbtn = document.getElementsByClassName("add")
-const products = []
-for (var i =0; i < addbtn.length; i++){
-    let cartbtn = addbtn[i]
-    cartbtn.addEventListener('click', ()=> {
-        let product = {
-            image: event.target.parentElement.parentElement.children[0].children[0].src,
-            title: event.target.parentElement.parentElement.children[1].children[0].textContent,
-            price: event.target.parentElement.parentElement.children[1].children[1].textContent,
-        }
-        alert('added to cart')
-        addToStorage(product)
-        displayCart()
-        subtotal()
-    })
+const addbtn = document.getElementsByClassName("add");
+const products = [];
+for (var i = 0; i < addbtn.length; i++) {
+  let cartbtn = addbtn[i];
+  cartbtn.addEventListener("click", () => {
+    let product = {
+      image:
+        event.target.parentElement.parentElement.children[0].children[0].src,
+      title:
+        event.target.parentElement.parentElement.children[1].children[0]
+          .textContent,
+      price: Number(
+        event.target.parentElement.parentElement.children[1].children[1].textContent.replace(
+          /\$/g,
+          ""
+        )
+      ),
+      quantity: 1,
+    };
+    alert("added to cart");
+    addToStorage(product);
+    displayCart();
+    subtotal();
+  });
 }
 
 //adding to local storage
-function addToStorage(product){
-    let cartItem = JSON.parse(localStorage.getItem("products"))
-    if (cartItem === null){
-        products.push(product)
-        localStorage.setItem("products", JSON.stringify(products))
-    }
-    else {
-        products.push(product)
-    }
-
-    localStorage.setItem("products", JSON.stringify(products))
+function addToStorage(product) {
+  let cartItem = JSON.parse(localStorage.getItem("products"));
+  if (cartItem === null) {
+    products.push(product);
+    localStorage.setItem("products", JSON.stringify(products));
+  } else {
+    let isItemNew = true;
+    cartItem = cartItem.map((prod) => {
+      if (prod.title == product.title) {
+        isItemNew = false;
+        const result = {
+          image: product.image,
+          price: product.price,
+          quantity: prod.quantity + 1,
+          title: product.title,
+        };
+        return result;
+      } else return prod;
+    });
+    if (isItemNew) cartItem.push(product);
+    localStorage.setItem("products", JSON.stringify(cartItem));
+  }
 }
 
+const quantityIncrease = (id, val) => {
+  try {
+      if (val <= 1) alert("You have to select at least 1 quantity or remove item from cart");
+    // get products from local storage
+    const cart = JSON.parse(localStorage.getItem("products"));
+    // update quantity of product
+    const updateCart = cart.map((prod, index) => {
+      return index === Number(id)
+        ? {
+            title: prod.title,
+            image: prod.image,
+            price: prod.price,
+            quantity: prod.quantity > 0 && val > 0 ? val : prod.quantity,
+          }
+        : prod;
+    });
+    // update local storage products
+    localStorage.setItem("products", JSON.stringify(updateCart));
+    // empty innerHTMl of cart items
+    document.getElementById("cart-items").innerHTML = "";
+    // display cart
+    displayCart();
+  } catch (error) {
+    // console.trace(error.message)
+  }
+};
 
 //displaying to the cart-page
-function displayCart(){
-    const cartdisplay = document.getElementById("cart-items")
-    let html = ""
-    let cartitem = JSON.parse(localStorage.getItem("products"))
-    cartitem.forEach(item => {
-    html += `
-        <div class="cart-row">
-        <div class="cart-item cart-column">
-            <img class="cart-item-image" src="${item.image}" width="100" height="100">
-            <span class="cart-item-title">${item.title}</span>
-        </div>
-        <span class="cart-price cart-column">${item.price} </span>
-        <div class="cart-qty cart-column">
-            <input class="cart-quantity-input" type="number" value="2">
-            <button class="btn-remove" type="button">remove</button>
-        </div>
-    </div>`
-    })
-    cartdisplay.innerHTML = html
-    console.log(cartitem)
+function displayCart() {
+  try {
+    const cartdisplay = document.getElementById("cart-items");
+    let html = "",
+      total = 0;
+    let cartitem = JSON.parse(localStorage.getItem("products"));
+    cartitem.forEach((item, index) => {
+      total += item.price * item.quantity;
+      html += `
+                <div class="cart-row" id="${index}">
+                    <div class="cart-item cart-column">
+                        <img class="cart-item-image" src="${
+                          item.image
+                        }" width="100" height="100">
+                        <span class="cart-item-title">${item.title}</span>
+                    </div>
+                    <div class="cart-price cart-column">$${item.price} </div>
+                    <div class="cart-column">$${
+                      item.price * item.quantity
+                    } </div>
+                    <div class="cart-qty cart-column">
+                        <input class="cart-quantity-input" type="number" value="${
+                          item.quantity
+                        }" onchange="quantityIncrease(${index}, this.value)">
+                        <button onclick="quantityIncrease(${index}, ${
+        item.quantity + 1
+      })">+</button>
+                        <button onclick="quantityIncrease(${index}, ${
+        item.quantity - 1
+      })">-</button>
+                        <button class="btn-remove" type="button" onclick="removeCartItem(${index})">remove</button>
+                    </div>
+                </div>
+                `;
+    });
+    cartdisplay.innerHTML = html;
+    document.getElementsByClassName(
+      "cart-total-price"
+    )[0].innerHTML = `$${total}`;
+  } catch (error) {
+    // console.trace(error.message);
+  }
 }
-displayCart()
-
-
-//function for subtotal
-function subtotal(){
-    let sum = 0
-    var cart = document.getElementsById("cart-items")[0]
-    var cartRows = cart.getElementsByClassName("cart-row")
-    for (var i =0; i< cartRows.length; i++){
-       var cartRow = cartRows[i]
-       var price = cartRow.getElementsByClassName("cart-price")[0]
-       var quantity = cartRow.getElementsByClassName("cart-quantity")[0]
-
-       var prices = parseInt(price.innerText.replace('$', ''))
-       var qty = quantity.value
-       sum = prices * qty
-   }
-   document.getElementsByClassName("cart-sub")[0].innerText = '$' + sum
-}
-
-//updates the cart total
-function updateCartTotal(){
-   var cart = document.getElementsById("cart-items")[0]
-   var cartRows = cart.getElementsByClassName("cart-row")
-   var total = 0
-   for (var i =0; i< cartRows.length; i++){
-       var cartRow = cartRows[i]
-       var price = cartRow.getElementsByClassName("cart-price")[0]
-       var quantity = cartRow.getElementsByClassName("cart-quantity")[0]
-
-       var prices = parseInt(price.innerText.replace('$', ''))
-       var qty = quantity.value
-       total = total + (prices * qty)
-   }
-   total = Math.round(total * 100)/ 100
-   document.getElementsByClassName("cart-total-price")[0].innerText = '$' + total
-}
-
+displayCart();
